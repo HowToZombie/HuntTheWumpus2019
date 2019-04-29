fd7e3a195cd10b35690f775e08f373f8060ea0e3
Erick Wang <1037742@apps.nsd.org>
2019-04-29 10:06:00 -0700

    first test

---
+++ GameLocations/GameLocationHuntTheWumpus/GameLocationHuntTheWumpus/GameLocation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GameLocationHuntTheWumpus
+{
+    public class GameLocation
+    {
+
+       // •	Store and interact with the cave used for this game
+//•	Keep track of where the hazards are
+        public int HazardLocation { get; private set; }
+//•	Keep track of where the Wumpus is. This includes controlling Wumpus behavior(that is, asleep, awake, moving).
+        public int WumpusLocation { get; private set; }
+//•	Keep track of where the player is
+        public int PlayerLocation { get; private set; }
+  
+//•	Control arrow shooting.
+//•	Give any necessary warnings
+//•	Obtain hints to help the player
+
+
+    }
+}
