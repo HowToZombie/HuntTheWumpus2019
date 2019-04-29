510c9876da1f18a42c2e487c4107fef77f1a30e9
Sam Friesen <1088290@apps.nsd.org>
2019-04-29 10:16:50 -0700

    first push

---
+++ Trivia/TriviaClassPrac/TriviaClassPrac/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TriviaClassPrac
+{
+    public class Class1
+    {
+        public int TriviaQuestions { get; private set; }
+        public int Answers { get; private set; }
+
+
+        public void AskQuestion()
+        {
+
+        }
+
+        public void DisplayAnswers()
+        {
+
+        }
+
+        public void twoOfthreeTrvia()
+        {
+           
+        }
+
+        public void threeOffiveTrivia()
+        {
+            
+        }
+    }
+   
+}
